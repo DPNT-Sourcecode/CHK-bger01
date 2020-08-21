4b8f8878163edb2c08ad6e851b8e584e45672470
sbx_user1051 <sbx_user1051@169.254.107.213>
2020-08-21 21:20:58 +0000

    Fri Aug 21 21:20:58 UTC 2020

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -1,28 +1,29 @@
 package befaster.solutions.SUM;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
-import accelerate.io.App;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
+@TestInstance(Lifecycle.PER_CLASS)
 public class SumSolutionTest {
-    private SumSolution sum;
+	private SumSolution sum;
 
-    @Before
-    public void setUp() {
+	@BeforeAll
+	public void setUp()
+	{
+		sum = new SumSolution();
+	}
 
-        sum = new SumSolution();
-    }
-
-    @Test
-    public void compute_sum() {
-        assertThat(sum.compute(1, 1), equalTo(2));
+	@Test
+	public void compute_sum()
+	{
+		assertThat(sum.compute(1, 1), equalTo(2));
 		assertNull(sum.compute(-1, 0), "Param 0-100 check");
 		assertNull(sum.compute(0, -1), "Param 0-100 check");
 		assertNull(sum.compute(101, 0), "Param 0-100 check");
@@ -30,5 +31,6 @@ public class SumSolutionTest {
 		assertEquals(1, sum.compute(1, 0), "Working params");
 		assertEquals(100, sum.compute(0, 100), "Working params");
 		assertEquals(42, sum.compute(40, 2), "Working params");
-    }
+	}
 }
+
