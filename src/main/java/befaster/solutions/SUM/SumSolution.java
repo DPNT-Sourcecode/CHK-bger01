1b8aa824ade179ae61187deab9790ecf33a44897
sbx_user1051 <sbx_user1051@169.254.107.213>
2020-08-21 20:50:58 +0000

    Fri Aug 21 20:50:58 UTC 2020

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,12 +1,12 @@
 package befaster.solutions.SUM;
 
-import befaster.runner.SolutionNotImplementedException;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SumSolution {
 
-    public Integer compute(int x, int y) {
+	public Integer compute(int x, int y)
+	{
 		Integer result = null;
 		if (x < 0 || x > 100 || y < 0 || y > 100) {
 			log.info("Paramters must be 0-100");
@@ -19,3 +19,4 @@ public class SumSolution {
 	}
 
 }
+
