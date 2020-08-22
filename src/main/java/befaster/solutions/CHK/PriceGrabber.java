58477a6d605f44b8dc8060cbcaa83bad8eed7639
sbx_user1051 <sbx_user1051@169.254.108.85>
2020-08-22 20:09:48 +0000

    Sat Aug 22 20:09:48 UTC 2020

--- src/main/java/befaster/solutions/CHK/PriceGrabber.java
+++ src/main/java/befaster/solutions/CHK/PriceGrabber.java
@@ -1,5 +1,29 @@
 package befaster.solutions.CHK;
 
-public class PriceGrabber {
+public final class PriceGrabber {
+	
+	public static Integer getUnitPrice(char cha) {
+		Integer price = null;
+		switch (cha) {
+			case 'A': {
+				price =  50;
+				break;
+			}
+			case 'B': {
+				price =  30;
+				break;
+			}
+			case 'C': {
+				price =  20;
+				break;
+			}
+			case 'D': {
+				price =  15;
+				break;
+			}
+		}
+		return price;
+	}
 
 }
+
---
+++ src/test/java/befaster/solutions/CHK/PriceGrabberTest.java
@@ -0,0 +1,15 @@
+package befaster.solutions.CHK;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PriceGrabberTest {
+
+	@Test
+	void testGetUnitPrice()
+	{
+		fail("Not yet implemented");
+	}
+
+}
