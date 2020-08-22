4a6131c957e3d077700ce7480a4d12f594c728b0
sbx_user1051 <sbx_user1051@169.254.108.85>
2020-08-22 20:06:48 +0000

    Sat Aug 22 20:06:48 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -7,3 +7,8 @@ public class CheckoutSolution {
         throw new SolutionNotImplementedException();
     }
 }
+
+Where:
+ - param[0] = a String containing the SKUs of all the products in the basket eg "ABBD"
+ - @return = an Integer representing the total checkout value of the items 
+
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -1,15 +1,35 @@
 package befaster.solutions.CHK;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
+@TestInstance(Lifecycle.PER_CLASS)
 class CheckoutSolutionTest {
+	
+	private CheckoutSolution checkoutSolution;
+
+	@BeforeAll
+	public void setUp()
+	{
+		checkoutSolution = new CheckoutSolution();
+	}
 
 	@Test
-	void testCheckout()
+	public void testHello()
 	{
-		fail("Not yet implemented");
+		// Assert valid return values
+		assertEquals(-1, checkoutSolution.checkout(null));
+		assertEquals(-1, checkoutSolution.checkout(""));
+		assertEquals(-1, checkoutSolution.checkout("1"));
+		assertThat(checkoutSolution.checkout("ABBC"), greaterThan(-1));
 	}
 
 }
+
