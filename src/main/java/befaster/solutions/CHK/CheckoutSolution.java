2652f507f301fed12432b2de9f5965c3f630208d
sbx_user1051 <sbx_user1051@169.254.108.85>
2020-08-22 21:12:48 +0000

    Sat Aug 22 21:12:48 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,13 +1,10 @@
 package befaster.solutions.CHK;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class CheckoutSolution {
-    public Integer checkout(String skus) {
-        throw new SolutionNotImplementedException();
-    }
+	
+	public Integer checkout(String skus)
+	{
+		return PriceGrabber.getPrice(skus);
+	}
+	
 }
-
-Where:
- - param[0] = a String containing the SKUs of all the products in the basket eg "ABBD"
- - @return = an Integer representing the total checkout value of the items 
