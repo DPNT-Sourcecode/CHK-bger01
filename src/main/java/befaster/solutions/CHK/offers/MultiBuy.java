4b3db52bdb8a3eb2cf1493140e95896d3b32cce4
sbx_user1051 <sbx_user1051@169.254.201.173>
2020-08-23 21:20:00 +0000

    Sun Aug 23 21:20:00 UTC 2020

--- src/main/java/befaster/solutions/CHK/offers/MultiBuy.java
+++ src/main/java/befaster/solutions/CHK/offers/MultiBuy.java
@@ -9,11 +9,15 @@ public class MultiBuy extends MultiBuyOffer{
 	private int numRequired;
 	private int totalPrice;
 	private int charPriority;
-	
+
 	public MultiBuy(Character item, int numRequired, int totalPrice, int charPriority) {
 		super();
+		this.item = item;
+		this.numRequired = numRequired;
+		this.totalPrice = totalPrice;
+		this.charPriority = charPriority;
 	}
-
+	
 	public boolean isApplicable(String skus)
 	{
 		return false;
