59daf13aefc2d2a26e7f63c6ff3b6e68d8732076
sbx_user1051 <sbx_user1051@169.254.198.13>
2020-08-24 13:21:37 +0000

    Mon Aug 24 13:21:37 UTC 2020

--- src/test/java/befaster/solutions/CHK/PriceGrabberTest.java
+++ src/test/java/befaster/solutions/CHK/PriceGrabberTest.java
@@ -64,6 +64,9 @@ class PriceGrabberTest {
 		assertThat(PriceGrabber.getPrice("YAZK"), is(70 + 50 + 20 + 21));
 		assertThat(PriceGrabber.getPrice("YAZKS"), is(70 + 50 + 45));
 		assertThat(PriceGrabber.getPrice("KKKK"), is(240));
+//		assertThat(PriceGrabber.getPrice("VV"), is(90));
+		assertThat(PriceGrabber.getPrice("VVVVV"), is(220));
 	}
 }
+
 
